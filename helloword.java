18ac10d0be289833424104dd25c903017e63e620
deepa_interview <59685544+deepa-project@users.noreply.github.com>
2020-01-11 06:09:17 -0500

    Create helloword.java

---
+++ helloword.java
@@ -0,0 +1,8 @@
+package com.deepa-project
+public class HelloWorld
+  {
+    public static void main(String[] args)
+        {
+          System.out.println("Hello world! Deepa, welcome to github!");
+        }
+   }     
